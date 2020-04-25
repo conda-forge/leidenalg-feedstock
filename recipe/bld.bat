87ecf9cbe2e281d2aa5d117699b59f500947c747
vtraag <vincent@traag.net>
2020-04-25 23:15:11 +0200

    Add pip (needed for Windows it seems) as build dependency, and setuptools as runtime dependency.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 set MSYSTEM=MINGW64
 set IGRAPH_EXTRA_CONFIGURE_ARGS="--disable-graphml"
-python -m pip install --no-deps --ignore-installed --no-build-isolation .
+python -m pip install . --no-deps -vv --no-build-isolation
