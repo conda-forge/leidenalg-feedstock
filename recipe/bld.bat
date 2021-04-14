4277c5a8ca40425bee85c6f9a45044c9e4c361a7
Vincent Traag <vincent@traag.net>
2021-04-14 16:16:11 +0200

    Build in separate build scripts and pass correct include/lib dirs.

---
+++ recipe/bld.bat
@@ -0,0 +1,5 @@
+SET IGRAPH_EXTRA_INCLUDE_PATH=%LIBRARY_INC%\igraph
+SET IGRAPH_EXTRA_LIBRARY_PATH=%LIBRARY_LIB%
+
+%PYTHON% -m pip install . -vv --install-option='--external'
+if errorlevel 1 exit 1
