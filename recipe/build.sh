6c7a632ebc9650c44f14dbba17aec79d89dd5dca
Vincent Traag <vincent@traag.net>
2021-04-14 16:54:32 +0200

    Use linux style env variable.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,4 @@
-export IGRAPH_EXTRA_INCLUDE_PATH=%CONDA_PREFIX%/include/igraph
-export IGRAPH_EXTRA_LIBRARY_PATH=%CONDA_PREFIX%/lib
+export IGRAPH_EXTRA_INCLUDE_PATH=${CONDA_PREFIX}/include/igraph
+export IGRAPH_EXTRA_LIBRARY_PATH=${CONDA_PREFIX}/lib
 
-%PYTHON% -m pip install . -vv --install-option='--external'
-if errorlevel 1 exit 1
+$PYTHON -m pip install . -vv --install-option="--external"
