d3e0d37f1be646ef5d454a61fee43e56d86b78b1
Vincent Traag <vincent@traag.net>
2021-04-14 17:08:25 +0200

    Debug paths.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 export IGRAPH_EXTRA_INCLUDE_PATH=${CONDA_PREFIX}/include/igraph
 export IGRAPH_EXTRA_LIBRARY_PATH=${CONDA_PREFIX}/lib
 
+echo "Extra include path: ${IGRAPH_EXTRA_INCLUDE_PATH}"
+echo "Extra library path: ${IGRAPH_EXTRA_LIBRARY_PATH}"
+
 $PYTHON -m pip install . -vv --install-option="--external"
