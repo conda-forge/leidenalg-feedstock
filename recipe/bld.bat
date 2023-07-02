a14130927915178d10a1c17934139716f62edda8
Vincent Traag <vincent@traag.net>
2023-07-02 23:40:32 +0200

    Remove build scripts, add libleidenalg dependency.

--- recipe/bld.bat
+++
@@ -1,5 +0,0 @@
-SET IGRAPH_EXTRA_INCLUDE_PATH=%LIBRARY_INC%\igraph
-SET IGRAPH_EXTRA_LIBRARY_PATH=%LIBRARY_LIB%
-
-%PYTHON% -m pip install . -vv --install-option="--external"
-if errorlevel 1 exit 1
